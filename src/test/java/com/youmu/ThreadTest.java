e620d8dc53785aef0c5d837afb0b6f69169bb6f9
wyoumuw <wyoumuw@gmail.com>
2018-07-18 09:18:18 +0900

    test for Adder

--- src/test/java/com/youmu/ThreadTest.java
+++ src/test/java/com/youmu/ThreadTest.java
@@ -1,9 +1,13 @@
 package com.youmu;
 
+import org.junit.Test;
+
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
 
 /**
  * @Author: YLBG-LDH-1506
@@ -38,5 +42,30 @@ public class ThreadTest {
 		synchronized (obj){
 			System.out.print("in");
 		}
+	}
+    static LongAdder longAdder=new LongAdder();
+	@Test
+	public void testAdder() throws InterruptedException {
+		ExecutorService executorService=Executors.newFixedThreadPool(20);
+		for (int i = 0; i < 40; i++) {
+			executorService.submit(()->{
+				longAdder.increment();
+				long l=longAdder.sum();//you can see same value be print when you set a debug point on inner of sum() method
+				System.out.println(l);
+			});
+		}
+		executorService.awaitTermination(10000, TimeUnit.MILLISECONDS);
+	}
+
+	AtomicLong atomicLong=new AtomicLong(0);
+	@Test
+	public void testAtomic() throws InterruptedException {
+		ExecutorService executorService=Executors.newFixedThreadPool(20);
+		for (int i = 0; i < 40; i++) {
+			executorService.submit(()->{
+				System.out.println(atomicLong.incrementAndGet());
+			});
+		}
+		executorService.awaitTermination(10000, TimeUnit.MILLISECONDS);
 	}
 }
